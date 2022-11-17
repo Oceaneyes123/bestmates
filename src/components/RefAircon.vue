ee13f7ecf03ae07882b8b48e316183d17bff0fa4
Jezrel Dave Sondia <jezreldavesondia@gmail.com>
2022-11-17 15:26:45 +0800

    initial clean commit

---
+++ src/components/RefAircon.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container class="d-flex flex-column justify-center">
+      <span class="text-h3 text-center mb-5 mt-16">IN PROGRESS</span>
+      <span class="text-h6 text-center">Website will be updated in few days</span>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+   
+  }),
+};
+</script>
